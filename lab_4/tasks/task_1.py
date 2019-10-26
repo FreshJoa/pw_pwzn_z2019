371da1f5dc271ea3fe9152d53a1646e0ecbc0ff0
Asia <mazureek6@gmail.com>
2019-10-26 13:18:24 +0200

    lab 4 - rozwiązania

--- lab_4/tasks/task_1.py
+++ lab_4/tasks/task_1.py
@@ -10,16 +10,16 @@ użyj wartość z pamięci kalkulatora. Obsłuż przypadki skrajne.
 
 class Calculator:
     def __init__(self):
-        self.memory = None
+        self._memory = None
         # Podpowiedz: użyj atrybutu do przechowywania wyniku
         # ostatniej wykonanej operacji, tak by metoda memorize przypisywała
         # wynik zapisany w tym atrybucie
 
-        #short_memory uzywamy gdy uzytokonik nie podał argunetu i wykonujemy
-        #operacje z poprzednią wartością
-        #zasze zapamietuje ostatni wynik
+        # short_memory uzywamy gdy uzytokonik nie podał argunetu i wykonujemy
+        # operacje z poprzednią wartością
+        # zasze zapamietuje ostatni wynik
         self._short_memory = None
-        self.operations_dict = {'+': self.__add__, }
+        self.operations_dict = {'+': self.__add__, '-': self.__sub__, '*': self.__mul__, '/': self.__divmod__}
 
     def __add__(self, arg1, arg2):
         return arg1 + arg2
@@ -28,12 +28,12 @@ class Calculator:
         return arg1 - arg2
 
     def __divmod__(self, arg1, arg2):
-        return arg1 /arg2
+        return arg1 / arg2
 
+    def __mul__(self, arg1, arg2):
+        return arg1 * arg2
 
-
-
-    def run(self, operator, arg1, arg2):
+    def run(self, operator, arg1, arg2=None):
         """
         Returns result of given operation.
 
@@ -46,23 +46,26 @@ class Calculator:
         :return: result of operation
         :rtype: float
         """
+
+        '''Obsługa przypadkøw wyjætkowych  - zwraca None'''
         if self.operations_dict.get(operator):
-            return ()
+            if arg2 is None and self._short_memory is not None:
+                self._short_memory = self.operations_dict.get(operator)(arg1, self._short_memory)
+            elif arg2:
+                self._short_memory = self.operations_dict.get(operator)(arg1, arg2)
 
-
-        raise NotImplementedError
+        return self._short_memory
 
     def memorize(self):
-        """Saves last operation result to memory."""
-        raise NotImplementedError
+        self._memory = self._short_memory
 
     def clean_memory(self):
         """Cleans memorized value"""
-        raise NotImplementedError
+        self._memory = None
 
     def in_memory(self):
         """Prints memorized value."""
-        print(f"Zapamiętana wartość: {self.memory}")
+        print(f"Zapamiętana wartość: {self._memory}")
 
 
 if __name__ == '__main__':
--- lab_4/tasks/task_2.py
+++ lab_4/tasks/task_2.py
@@ -1,3 +1,5 @@
+import math
+
 """
 Częśćć 1 (1 pkt): Uzupełnij klasę Vector tak by reprezentowała wielowymiarowy wektor.
 Klasa posiada przeładowane operatory równości, dodawania, odejmowania,
@@ -11,10 +13,64 @@ Wszystkie metody sprawdzają wymiar.
 """
 
 
+def check_dim(dim1, dim2):
+    try:
+        if dim1 is not dim2:
+            raise ValueError
+        return True
+    except ValueError:
+        print("Niezgodność wymiarów")
+
+
 class Vector:
-    dim = None  # Wymiar vectora
+
     def __init__(self, *args):
-        raise NotImplemented
+        self.vector = tuple(args)
+        self._dim = len(self.vector)
+
+    @property
+    def dim(self):
+        return self._dim
+
+    def __eq__(self, other):
+        return self.dim == other.dim and self.vector == other.vector
+
+    def __add__(self, other):
+        check_dim(self.dim, other.dim)
+        response_vector = []
+        for i in range(0, self.dim):
+            response_vector.append(self.vector[i] + other.vector[i])
+
+        return Vector(*response_vector)
+
+    def __sub__(self, other):
+        check_dim(self.dim, other.dim)
+        response_vector = []
+        for i in range(0, self.dim):
+            response_vector.append(self.vector[i] - other.vector[i])
+
+        return Vector(*response_vector)
+
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            response_vector = [element * other for element in self.vector]
+            return Vector(*response_vector)
+        else :
+            check_dim(self.dim, other.dim)
+            response_int = 0
+            for i in range(0, self.dim):
+                response_int += self.vector[i]*other.vector[i]
+            return response_int
+
+    def __len__(self):
+        response_len = 0.0
+        for element in self.vector:
+            response_len += element**2
+
+        return int(math.sqrt(response_len))
+
+
+
 
     @staticmethod
     def calculate_vector(beg, end):
@@ -28,14 +84,22 @@ class Vector:
         :return: Calculated vector
         :rtype: tuple
         """
-        raise NotImplemented
+        check_dim(len(beg), len(end))
+        response_points_list = []
+        for i in range(0, len(beg)):
+            response_points_list.append(end[i] - beg[i])
+
+        return tuple(response_points_list)
+
+
+
 
     @classmethod
     def from_points(cls, beg, end):
         """"""
         """
         Generate vector from given points.
-
+    
         :param beg: Begging point
         :type beg: list, tuple
         :param end: End point
@@ -43,16 +107,20 @@ class Vector:
         :return: New vector
         :rtype: tuple
         """
-        raise NotImplemented
+        vector = cls.calculate_vector(beg, end)
+        return Vector(*vector)
+
 
 
 if __name__ == '__main__':
-    v1 = Vector(1,2,3)
-    v2 = Vector(1,2,3)
-    assert v1 + v2 == Vector(2,4,6)
+    v1 = Vector(1, 2, 3)
+    v2 = Vector(1, 2, 3)
+    assert v1 + v2 == Vector(2, 4, 6)
     assert v1 - v2 == Vector(0,0,0)
     assert v1 * 2 == Vector(2,4,6)
     assert v1 * v2 == 14
-    assert len(Vector(3,4)) == 5.
+    #Jak zrobić żeby len zwracało float??
+    assert len(Vector(3,4)) == 5
+
     assert Vector.calculate_vector([0, 0, 0], [1,2,3]) == (1,2,3)
     assert Vector.from_points([0, 0, 0], [1,2,3]) == Vector(1,2,3)
